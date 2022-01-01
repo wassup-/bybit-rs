890df6d5967874330ac7d96231ecccda60c7c310
Admin <72892531+peepopoggers@users.noreply.github.com>
2022-01-01 22:31:56 +0000

    added thiserror

--- src/ws/error.rs
+++ src/ws/error.rs
@@ -16,7 +16,7 @@ pub enum Error {
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "Channel: {}", self)
+        write!(f, "Ws: {}", self)
     }
 }
 
